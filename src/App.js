49f774ce1c5b85d2c1f1479205bc7cb0703476a9
dawoori <daul@kookmin.ac.kr>
2020-02-07 15:34:29 +0900

    class component, setState

--- src/App.js
+++ src/App.js
@@ -1,62 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const imageOfCity = [
-  {
-    id: 1,
-    name: "Seoul",
-    image: "https://img.theculturetrip.com/x/smart/wp-content/uploads/2013/07/lotte-world-tower-1791802_1280.jpg",
-    
-  },
-  {
-    id: 2,
-    name: "Seosan",
-    image: "https://img-wishbeen.akamaized.net/plan/1457487355490_samhwa.png",
-    rating: 4
-  },
-  {
-    id: 3,
-    name: "Korea",
-    image: "https://lp-cms-production.imgix.net/2019-06/09a64fea2933f6da77ab07d671d1f678-south-korea.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4",
-    rating: 3
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+
+  min = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>I'm a class component {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.min}>Min</button>
+      </div>
+    );
   }
-];
-
-// function renderCity(city) {
-//   console.log();
-//   return <Image name={city.name} picture={city.image} />
-// }
-
-function Image({ name, picture, rating }) {
-  return (
-    <div>
-      <h3>I'm the {name}.</h3>
-      <p>{rating}</p>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Image.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      <h1>Hello.</h1>
-      {imageOfCity.map(city => (
-        <Image
-          key={city.id}
-          name={city.name}
-          picture={city.image}
-          rating={city.rating}
-        />
-      ))}
-    </div>
-  );
 }
 
 export default App;
